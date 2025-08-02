0ff6f19250d3ce59987250a233cb9b5f7f916d98
Ali Heydari <ali4heydari@gmail.com>
2025-08-02 18:51:16 +0330

    refactor: overall enhancements

---
+++ .release-it.mjs
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import {Config} from "release-it";
+
+/**
+ * @type {Config}
+ * */
+const config = {
+  git: {
+    commitMessage: "chore(release): bump version to v${version}",
+    tagAnnotation: "Release v${version}",
+    tagName: "v${version}",
+    push: false,
+    requireUpstream: false,
+    getLatestTagFromAllRefs: true,
+  },
+  npm: {
+    publish: false,
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      preset: {
+        name: "conventionalcommits",
+      },
+      infile: "CHANGELOG.md",
+    },
+    "@j-ulrich/release-it-regex-bumper": {
+      out: [
+        {
+          files: ["spec/openapi.yaml", "redocly.config.yaml", "README.md", "README.md", "README.md"],
+          search: "{{version}}",
+          replace: "{{version}}",
+        },
+      ]
+    },
+  },
+};
+
+export default config;
---
+++ lint-staged.config.js
@@ -0,0 +1,3 @@
+export default {
+  "*.yaml": ["eslint --fix"],
+};
